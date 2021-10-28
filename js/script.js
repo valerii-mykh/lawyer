c4580139ef9ab8494895e46014f63f01f0628349
valerii11 <valerii_mih@ukr.net>
2021-10-28 14:18:20 +0300

    create

---
+++ js/script.js
@@ -0,0 +1,19 @@
+const $ = window.jQuery;
+$('.owl-carousel').owlCarousel({
+    loop: true,
+    margin: 20,
+    nav: true,
+    autoplay: true,
+    autoplayTimeout: 3000,
+    responsive: {
+        0: {
+            items: 1
+        },
+        600: {
+            items: 2
+        },
+        1000: {
+            items: 3
+        }
+    }
+})
